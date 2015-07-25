6bedfd1b1e6561d7720b9a0ae6a181fabca13cfd
Owens O'Brien <owensobrien@gmail.com>
2015-07-25 14:35:43 -0400

    manage the loading of configuration files

--- config/express.js
+++ config/express.js
@@ -8,10 +8,10 @@ module.exports = function() {
     var app = express();
 
     if (process.env.NODE_ENV === 'development') {
-    	console.log("running in development mode");
+    	console.log("Running in development mode!");
     	app.use(morgan('dev'));
     } else if (process.env.NODE_ENV === 'production') {
-    	console.log("running in production mode");
+    	console.log("Running in production mode!");
     	app.use(compress());
     }
 
