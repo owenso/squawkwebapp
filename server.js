f5eca7e493ad058e12697963cca72eee3f81c311
Owens O'Brien <owensobrien@gmail.com>
2015-07-25 13:36:59 -0400

    configure express object

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-var express = require('express');
+var express = require('./config/express');
 var app = express();
 
-
-
-app.use('/', function (request, response){
-  response.send('Hi GUYS!');
-});
-
 app.listen(3000);
+module.exports = app;
 
 console.log('Server running at port 3000');
 
 
 
-
-module.exports = app;
