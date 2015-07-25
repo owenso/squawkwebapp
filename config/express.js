3a89d71380b2917b28015a0a4ca7406529baab73
Owens O'Brien <owensobrien@gmail.com>
2015-07-25 14:24:47 -0400

    set environment variables and configuration

--- config/express.js
+++ config/express.js
@@ -1,7 +1,27 @@
-var express = require('express');
+var express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
-module.exports = function(){
-	var app = express();
-	require('../app/routes/index.server.routes.js')(app);
-	return app;
+module.exports = function() {
+    var app = express();
+
+    if (process.env.NODE_ENV === 'development') {
+    	console.log("running in development mode");
+    	app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {
+    	console.log("running in production mode");
+    	app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+    	extended:true
+    }));
+
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    require('../app/routes/index.server.routes.js')(app);
+    return app;
 };
