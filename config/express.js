393ce5f4246606368332a985247d905d199aa7d8
Owens O'Brien <owensobrien@gmail.com>
2015-11-07 16:44:16 -0500

    configure passport

--- config/express.js
+++ config/express.js
@@ -4,7 +4,8 @@ var config = require('./config'),
     compress = require('compression'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
-    session = require('express-session');
+    session = require('express-session'),
+    passport = require('passport');
 
 module.exports = function() {
     var app = express();
@@ -32,6 +33,9 @@ module.exports = function() {
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     require('../app/routes/index.server.routes.js')(app);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
+var passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
