e11e9a7b1b185219b35570e814de54a5520b8a5d
Owens O'Brien <owensobrien@gmail.com>
2015-11-14 15:56:52 -0500

    add show RESTFUL route

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -21,3 +21,20 @@ exports.list = function(req, res, next) {
         }
     });
 };
+
+exports.read = function(req, res) {
+    res.json(req.user);
+};
+
+exports.userByID = function(req, res, next, id) {
+    User.findOne({
+        _id: id
+    }, function(err, user) {
+        if (err) {
+            return next(err);
+        } else {
+            req.user = user;
+            next();
+        }
+    })
+}
