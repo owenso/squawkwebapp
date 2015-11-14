931684a24e262618fcc91362db714033a178db1a
Owens O'Brien <owensobrien@gmail.com>
2015-11-14 15:24:42 -0500

    add webstorm files to gitignore, set up index route

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,7 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-	app.route('/users').post(users.create);
-}
+	app.route('/users')
+		.post(users.create)
+		.get(users.list);
+};
