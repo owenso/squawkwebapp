7fbb98498e15ed679e07290319a37141220aa25f
Owens O'Brien <owensobrien@gmail.com>
2015-11-15 00:00:19 -0500

    user model getters, setters, and other functions

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,15 +4,33 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
+    email: {
+        type: String,
+        index: true,
+        match: /.+\@.+\..+/
+    },
     username: {
         type: String,
-        trim: true
+        trim: true,
+        unique: true,
+        required: true
     },
-    password: String,
+    password: {
+        type: String,
+        validate: [
+            function(password) {
+                return password.length >= 6;
+            },
+            'Password should be longer.'
+        ]
+    },
     created: {
         type: Date,
         default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
     },
     website: {
         type: String,
@@ -31,13 +49,26 @@ var UserSchema = new Schema({
 });
 
 UserSchema.virtual('fullName').get(function() {
-	return this.firstName + ' ' + this.lastName;
+    return this.firstName + ' ' + this.lastName;
 }).set(function(fullName) {
-	var splitName = fullName.split(' ');
-	this.firstName = splitName[0] || '';
-	this.lastName = splitName[1] || '';
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
 });
 
-UserSchema.set('toJSON', {getters: true, virtuals:true});
+UserSchema.statics.findOneByUsername = function(username, callback) {
+    this.findOne({
+        username: new RegExp(username, 'i')
+    }, callback);
+};
+
+UserSchema.methods.authenticate = function(password) {
+    return this.password === password;
+}
+
+UserSchema.set('toJSON', {
+    getters: true,
+    virtuals: true
+});
 
 mongoose.model('User', UserSchema);
