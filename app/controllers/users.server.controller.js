0cdd8b8ce2920d3f1afeb351876fe33fd19c7790
Owens O'Brien <owensobrien@gmail.com>
2015-11-14 21:35:03 -0500

    set update response to return new model

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -40,7 +40,7 @@ exports.userByID = function(req, res, next, id) {
 };
 
 exports.update = function(req, res, next) {
-    User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+    User.findByIdAndUpdate(req.user.id, req.body, {new: true}, function(err, user) {
         if (err) {
             return next(err);
         } else {
