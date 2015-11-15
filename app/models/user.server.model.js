a2ff5008e9ead4a6f9d4c6bf9383294512141594
Owens O'Brien <owensobrien@gmail.com>
2015-11-14 22:40:37 -0500

    getter and setter examples for user models

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,13 +5,39 @@ var UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
+    username: {
+        type: String,
+        trim: true
+    },
     password: String,
     created: {
-    	type: Date,
-    	default: Date.now,
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+
+                return url;
+            }
+        }
     }
 });
 
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {getters: true, virtuals:true});
 
 mongoose.model('User', UserSchema);
