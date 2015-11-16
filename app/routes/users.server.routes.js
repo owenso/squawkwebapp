d405c6b7fdc22a17226c1fc6e137ce440000b380
Owens O'Brien <owensobrien@gmail.com>
2015-11-15 20:14:43 -0500

    configure connect-flash module and wire routes for signup and signin

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,4 +1,5 @@
-var users = require('../../app/controllers/users.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+						passport = require('passport');
 
 module.exports = function(app) {
     app.route('/users')
@@ -13,4 +14,17 @@ module.exports = function(app) {
 
     app.param('userId', users.userByID);
 
+    app.route('/signup')
+    	.get(users.renderSignup)
+    	.post(users.signup);
+
+    app.route('/signin')
+    	.get(users.renderSignin)
+    	.post(passport.authenticate('local', {
+    		successRedirect: '/',
+    		failureRedirect: '/signin',
+    		failureFlash: true
+    	}));
+
+    app.get('/signout', users.signout);
 };
