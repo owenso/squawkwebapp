e685b0b3ac9da8093b8fdee4567f15d5f09b6531
Owens O'Brien <owensobrien@gmail.com>
2015-11-16 20:51:16 -0500

    configure facebook oauth strategy

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,10 @@
 module.exports = {
   // Development configuration options
   db: 'mongodb://localhost/squawker',
-  sessionSecret: 'thisguywhowrotethisbookisagenius'
-
+  sessionSecret: 'thisguywhowrotethisbookisagenius',
+  facebook: {
+  	clientId: '108032549563495',
+  	clientSecret: 'fd5a36493b2344d748b97a17ba3ea0be',
+  	callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
 };
