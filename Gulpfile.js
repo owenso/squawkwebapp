4aa97d95693d94d6ce5e8988e39b78cf87ce1df0
Owens O'Brien <owensobrien@gmail.com>
2015-12-16 04:52:18 -0500

    add livereload for gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+
 
 gulp.task('styles', function() {
     gulp.src('./public/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./public/css/'));
+        .pipe(gulp.dest('./public/css/'))
+        .pipe(livereload());
 });
 
 //Watch task for changes
 gulp.task('default',function() {
 		//watch this, [do this]
+		livereload.listen();
     gulp.watch('./public/scss/**/*.scss',['styles']);
 });
