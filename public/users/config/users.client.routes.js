0e54aa1495a6c9328380ce6866434d585c7a4406
Owens O'Brien <owensobrien@gmail.com>
2015-12-18 11:32:44 -0500

    first step of possible redirect solution

--- public/users/config/users.client.routes.js
+++ public/users/config/users.client.routes.js
@@ -1,9 +1,25 @@
 angular.module('users').config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
+
+
+        //http://code.ciphertrick.com/2014/12/14/check-condition-before-loading-route-in-angular-js/
+
+
+
         .when('/', {
             templateUrl: '/users/views/users.client.signin.html',
-            controller: 'UserController'
+            controller: 'UserController',
+            resolve: {
+                    "check": function($rootScope, $location) {
+                        if ($rootScope.authenticated) {
+                            console.log('ok');
+                        } else {
+                            $location.path('/'); //redirect user to home.
+                            alert("You don't have access here");
+                        }
+                    }
+                }
         })
 
         .when('/signup', {
