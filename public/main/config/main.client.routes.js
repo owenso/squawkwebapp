0f0dac419567a2855e01a835fb0118b5e4acbaee
Owens O'Brien <owensobrien@gmail.com>
2015-12-28 20:46:28 -0500

    main redirects to requests page

--- public/main/config/main.client.routes.js
+++ public/main/config/main.client.routes.js
@@ -2,7 +2,7 @@ angular.module('main').config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
         .when('/', {
-            templateUrl: '/main/views/main.client.requests.html'
+            redirectTo: '/requests'
         })
         .when('/profile', {
             templateUrl: '/main/views/users.client.userprofile.html',
