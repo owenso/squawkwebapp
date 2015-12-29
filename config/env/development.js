4f4bd2d7753d2537cdcd9a4ab35309350856d874
Owens O'Brien <owensobrien@gmail.com>
2015-12-29 00:45:29 -0500

    adding configurations for heroku

--- config/env/development.js
+++ config/env/development.js
@@ -1,11 +1,11 @@
 module.exports = {
-    urlRoot: 'https://squawk-staging.herokuapp.com/',
+    urlRoot: 'https://squawk-development.herokuapp.com/',
     db: process.env.DB, //no local fallback
     sessionSecret: process.env.SESSION_SECRET,
     facebook: {
         clientID: process.env.FACE_CLIENT_ID,
         clientSecret: process.env.FACE_CLIENT_SECRET,
-        callbackURL: 'https://squawk-staging.herokuapp.com/oauth/facebook/callback'
+        callbackURL: 'https://squawk-development.herokuapp.com/oauth/facebook/callback'
     },
     aws: {
         accessKeyID: process.env.AWS_ACCESS_KEY_ID,
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
