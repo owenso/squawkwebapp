c2734f728944e39155d8f824f207185781c97846
Owens O'Brien <owensobrien@gmail.com>
2015-12-28 20:40:52 -0500

    fixing favicon and button location

--- app/controllers/main.server.controller.js
+++ app/controllers/main.server.controller.js
@@ -1,8 +1,10 @@
 exports.render = function(req, res) {
 		console.log(JSON.stringify(req.user._id));
+		var urlRoot = require('../../config/config');
     res.render('main', {
         userImg: req.user.userImg,
-        user: JSON.stringify(req.user._id)
+        user: JSON.stringify(req.user._id),
+        root: urlRoot.urlRoot
     });
 
 };
