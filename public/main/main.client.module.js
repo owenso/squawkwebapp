8dcb0b7402fd85d00d6de57506558ceab2b851d4
Owens O'Brien <owensobrien@gmail.com>
2016-01-01 14:28:44 -0500

    basic css adjustments and angular code to determine request statuses

--- public/main/main.client.module.js
+++ public/main/main.client.module.js
@@ -1,4 +1,1 @@
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-console.log(navigator.getUserMedia);
-console.log(navigator.mozGetUserMedia);
 angular.module('main', []);
