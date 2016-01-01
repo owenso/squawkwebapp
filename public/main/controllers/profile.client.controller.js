b9a0742603a963faedff6d7187689f50cc192f61
Owens O'Brien <owensobrien@gmail.com>
2016-01-01 17:54:27 -0500

    allow profile page numbers to update dynamically

--- public/main/controllers/profile.client.controller.js
+++ public/main/controllers/profile.client.controller.js
@@ -5,6 +5,21 @@ angular.module('main').controller('ProfileController', ['$scope', 'MainService',
         MainService.getUser()
             .success(function(data) {
                 $scope.userData = data;
+                $scope.pend = 0;
+                $scope.ans = 0;
+                $scope.comp = 0;
+
+                for (var i = 0; i < data.createdRequestIds.length; i++) {
+                	if (data.createdRequestIds[i].pending === true){
+                		$scope.pend += 1;
+                	}
+                	if (data.createdRequestIds[i].completed === true){
+                		$scope.comp += 1;
+                	}
+                	if (data.createdRequestIds[i].responseMessageIds>0){
+                		$scope.ans += 1;
+                	}
+                }
             });
     };
 
