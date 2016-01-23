9884a817072788fd4131de8013181aa33ecec5ae
Owens O'Brien <owensobrien@gmail.com>
2016-01-23 13:04:16 -0500

    add gulpfile tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,23 +7,47 @@ var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var size = require('gulp-filesize');
+var print = require('gulp-print');
+var lib = require('bower-files')();
 
+var jsPaths = ['.public/main/*.js','.public/main/**/*.js','.public/users/*.js','.public/users/**/*.js','.public/request_modal/*.js','.public/request_modal/**/*.js','.public/application.js']
 
 gulp.task('styles', function() {
     gulp.src('./public/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./public/css/'))
+        .pipe(size())
         .pipe(livereload());
 });
 
-//Watch task for changes
-gulp.task('default',function() {
-		livereload.listen();
-		//watch this, [do this]
-    gulp.watch('./public/scss/**/*.scss',['styles']);
+gulp.task('lib', function () {
+    gulp.src(lib.ext('js').files)
+        .pipe(print())
+        .pipe(concat('lib.min.js'))
+        .pipe(size())
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'))
 });
 
-gulp.task('js', function() {
-	return gulp.src('.')
-	})
+gulp.task('js', function () {
+    gulp.src(jsPaths)
+        .pipe(print())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+
+        .pipe(gulp.dest('./public/js'))
+    })
+
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(jsPaths, ['js'])
+    gulp.watch(lib.ext('js').files, ['lib'])
+    gulp.watch('./public/scss/**/*.scss',['styles']);
+})
+
+gulp.task('default',['watch', 'lib', 'js', 'styles']);
