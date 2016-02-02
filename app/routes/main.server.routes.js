983e8d4931654a5f7654fa338ac17c42ea1fa168
Owens O'Brien <owensobrien@gmail.com>
2016-02-02 08:56:30 -0500

    signup/signin auth fix, code cleanup

--- app/routes/main.server.routes.js
+++ app/routes/main.server.routes.js
@@ -2,13 +2,6 @@ module.exports = function(app) {
     var main = require('../controllers/main.server.controller');
 
     function isMainAuthenticated(req, res, next) {
-
-        // //need to handle redirect to signup2
-        // if (!req.user) {
-        //     res.redirect('/');
-        // } else {
-        //     return next();
-        // }]
         if (!req.user) {
             res.redirect('../');
         } else if (req.user.nativeLanguages.length === 0) {
